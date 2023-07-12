f3a1879c28b63df874d6a946ec459fb3fa5f5d37
Gijs Vermeulen <gverm@inuits.eu>
2023-07-12 10:40:58 +0200

    Refs #114229 #114233 add loader & util functionalities

---
+++ src/elody/loader.py
@@ -0,0 +1,70 @@
+import os
+import util
+
+from importlib import import_module
+from inuits_policy_based_auth.exceptions import (
+    PolicyFactoryException,
+)
+
+
+def load_apps(flask_app):
+    apps = util.read_json_as_dict(os.getenv("APPS_MANIFEST"))
+    for app in apps:
+        for resource in apps[app].get("resources", []):
+            api_bp = import_module(f"apps.{app}.resources.{resource}").api_bp
+            flask_app.register_blueprint(api_bp)
+
+
+def load_policies(policy_factory, logger):
+    apps = util.read_json_as_dict(os.getenv("APPS_MANIFEST"))
+    for app in apps:
+        try:
+            auth_type = "authentication"
+            for policy_module_name in apps[app]["policies"].get(auth_type):
+                policy = __get_class(app, auth_type, policy_module_name)
+                policy = __instantiate_authentication_policy(
+                    policy_module_name, policy, logger
+                )
+                policy_factory.register_authentication_policy(f"apps.{app}", policy)
+            auth_type = "authorization"
+            for policy_module_name in apps[app]["policies"].get(auth_type):
+                policy = __get_class(app, auth_type, policy_module_name)
+                policy_factory.register_authorization_policy(f"apps.{app}", policy())
+            # FIXME: don't always set last app as fallback
+            policy_factory.set_fallback_key_for_policy_mapping(f"apps.{app}")
+        except Exception as error:
+            raise PolicyFactoryException(
+                f"Policy factory was not configured correctly: {str(error)}"
+            ).with_traceback(error.__traceback__)
+
+
+def load_queues():
+    apps = util.read_json_as_dict(os.getenv("APPS_MANIFEST"))
+    for app in apps:
+        try:
+            import_module(f"apps.{app}.resources.queues")
+        except ModuleNotFoundError:
+            pass
+
+
+def __get_class(app, auth_type, policy_module_name):
+    try:
+        module = import_module(f"apps.{app}.policies.{auth_type}.{policy_module_name}")
+    except:
+        module = import_module(
+            f"inuits_policy_based_auth.{auth_type}.policies.{policy_module_name}"
+        )
+    policy_class_name = module.__name__.split(".")[-1].title().replace("_", "")
+    policy = getattr(module, policy_class_name)
+    return policy
+
+
+def __instantiate_authentication_policy(policy_module_name, policy, logger):
+    if policy_module_name == "token_based_policies.authlib_flask_oauth2_policy":
+        return policy(
+            logger,
+            os.getenv("ROLE_SCOPE_MAPPING", "role_scope_mapping.json"),
+            os.getenv("STATIC_ISSUER"),
+            os.getenv("STATIC_PUBLIC_KEY"),
+        )
+    return policy()
---
+++ src/elody/util.py
@@ -0,0 +1,84 @@
+import json
+
+from cloudevents.conversion import to_dict
+from cloudevents.http import CloudEvent
+
+
+class NonUniqueException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+def __send_cloudevent(mq_client, routing_key, data):
+    attributes = {"type": routing_key, "source": "dams"}
+    event = to_dict(CloudEvent(attributes, data))
+    mq_client.send(event, routing_key=routing_key)
+
+
+def get_raw_id(item):
+    return item.get("_key", item["_id"])
+
+
+def get_item_metadata_value(item, key):
+    for item in item.get("metadata", []):
+        if item["key"] == key:
+            return item["value"]
+    return ""
+
+
+def mediafile_is_public(mediafile):
+    publication_status = get_item_metadata_value(mediafile, "publication_status")
+    return publication_status.lower() in ["beschermd", "expliciet", "publiek"]
+
+
+def read_json_as_dict(filename, logger):
+    try:
+        with open(filename) as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError) as ex:
+        logger.error(f"Could not read {filename} as a dict: {ex}")
+    return {}
+
+
+def signal_child_relation_changed(mq_client, collection, id):
+    data = {"parent_id": id, "collection": collection}
+    __send_cloudevent(mq_client, "dams.child_relation_changed", data)
+
+
+def signal_edge_changed(mq_client, parent_ids_from_changed_edges):
+    data = {
+        "location": f'/entities?ids={",".join(parent_ids_from_changed_edges)}&skip_relations=1'
+    }
+    __send_cloudevent(mq_client, "dams.edge_changed", data)
+
+
+def signal_entity_changed(mq_client, entity):
+    data = {
+        "location": f"/entities/{get_raw_id(entity)}",
+        "type": entity.get("type", "unspecified"),
+    }
+    __send_cloudevent(mq_client, "dams.entity_changed", data)
+
+
+def signal_entity_deleted(mq_client, entity):
+    data = {"_id": get_raw_id(entity), "type": entity.get("type", "unspecified")}
+    __send_cloudevent(mq_client, "dams.entity_deleted", data)
+
+
+def signal_mediafile_changed(mq_client, old_mediafile, mediafile):
+    data = {"old_mediafile": old_mediafile, "mediafile": mediafile}
+    __send_cloudevent(mq_client, "dams.mediafile_changed", data)
+
+
+def signal_mediafile_deleted(mq_client, mediafile, linked_entities):
+    data = {"mediafile": mediafile, "linked_entities": linked_entities}
+    __send_cloudevent(mq_client, "dams.mediafile_deleted", data)
