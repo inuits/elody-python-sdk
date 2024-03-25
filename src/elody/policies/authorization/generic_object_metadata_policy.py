93b962f090790ff0fe00894e93182c581054736e
Delsin Van Grembergen <delsin@inuits.eu>
2024-03-25 08:25:35 +0100

    Add build_user_context_for_anonymous_user and remove token checks.

--- src/elody/policies/authorization/generic_object_metadata_policy.py
+++ src/elody/policies/authorization/generic_object_metadata_policy.py
@@ -22,7 +22,7 @@ class GenericObjectMetadataPolicy(BaseAuthorizationPolicy):
         self, policy_context: PolicyContext, user_context: UserContext, request_context
     ):
         request: Request = request_context.http_request
-        if not user_context.auth_objects.get("token") or not regex.match(
+        if not regex.match(
             "^/[^/]+/[^/]+/metadata$", request.path
         ):
             return policy_context
