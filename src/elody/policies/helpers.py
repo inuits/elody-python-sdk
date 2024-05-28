79ab2133ae236674755a300d9541e18effd9816d
Eray Özcan <eray@inuits.eu>
2024-05-28 15:26:39 +0200

    refactor: use collection resolver in policy helper

--- src/elody/policies/helpers.py
+++ src/elody/policies/helpers.py
@@ -4,11 +4,8 @@ from flask_restful import abort  # pyright: ignore
 def get_item(storage_manager, user_context_bag, view_args):
     view_args = view_args or {}
     id = view_args.get("id")
-    collections = view_args.get(
-        "collection", user_context_bag.get("autosearch_collections", ["entities"])
-    )
-    if not isinstance(collections, list):
-        collections = [collections]
+    resolve_collections = user_context_bag["collection_resolver"]
+    collections = resolve_collections(collection=view_args.get("collection"), id=id)
     for collection in collections:
         if item := storage_manager.get_db_engine().get_item_from_collection_by_id(
             collection, id
