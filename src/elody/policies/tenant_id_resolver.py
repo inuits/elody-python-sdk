e0c75315db38054e8ee2580a2957e8a3d9b26388
Delsin Van Grembergen <delsin@inuits.eu>
2024-07-09 10:28:27 +0200

    fix: remove unnecessary exception

--- src/elody/policies/tenant_id_resolver.py
+++ src/elody/policies/tenant_id_resolver.py
@@ -317,7 +317,6 @@ class MediafileDetailDeleteRequest(BaseRequest):
             regex.match(r"^/mediafiles/(.+)$", request.path)
             and request.method == "DELETE"
         ):
-            raise Exception(self._get_tenant_id_from_mediafile(request.view_args.get("id")))
             return self._get_tenant_id_from_mediafile(request.view_args.get("id"))
         return None
 
