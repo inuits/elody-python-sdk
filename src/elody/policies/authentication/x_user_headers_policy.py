8dbc95357b47bbf554f05a0bc6e6877dc0ed47eb
Andy Indemans <andy.indemans@inuits.eu>
2025-10-02 14:52:33 +0200

    MR feedback

--- src/elody/policies/authentication/x_user_headers_policy.py
+++ src/elody/policies/authentication/x_user_headers_policy.py
@@ -1,9 +1,9 @@
-import os
+from configuration import get_object_configuration_mapper
 from flask import Request
 from inuits_policy_based_auth.authentication.base_authentication_policy import (
     BaseAuthenticationPolicy,
 )
-from configuration import get_object_configuration_mapper
+from os import getenv
 from storage.storagemanager import StorageManager
 
 
@@ -13,22 +13,22 @@ class XUserHeadersPolicy(BaseAuthenticationPolicy):
 
         auth_header = request.headers.get("Authorization", "")
         if auth_header.startswith("Bearer "):
-            token = auth_header[7:]  # Remove "Bearer " prefix
-            static_jwt = os.getenv("STATIC_JWT")
-            
+            token = auth_header.removeprefix("Bearer ")
+            static_jwt = getenv("STATIC_JWT")
+
             if static_jwt and token == static_jwt:
                 user_email = request.headers.get("X-User-Email")
                 if user_email:
                     config = get_object_configuration_mapper().get("user")
                     storage_manager = StorageManager()
-                    user = storage_manager.get_db_engine().get_item_from_collection_by_id(
-                        config.crud()["collection"], user_email
+                    user = (
+                        storage_manager.get_db_engine().get_item_from_collection_by_id(
+                            config.crud()["collection"], user_email
+                        )
                     )
-                    
+
                     if user:
                         user_context.id = user.get("_id", user.get("id"))
                         user_context.email = user_email
-                else:
-                    pass
-        
+
         return user_context
