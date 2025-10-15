c3ade6794c4092e924da09a223dd264d490eb2c8
Eray Özcan <eray@inuits.eu>
2025-10-15 11:39:01 +0200

    fix: except AttributeError while loading optional blueprints

--- src/elody/loader.py
+++ src/elody/loader.py
@@ -13,8 +13,11 @@ def load_apps(flask_app, logger):
     apps = util.read_json_as_dict(os.getenv("APPS_MANIFEST"), logger)
     for app in apps:
         for resource in apps[app].get("resources", []):
-            api_bp = import_module(f"apps.{app}.resources.{resource}").api_bp
-            flask_app.register_blueprint(api_bp)
+            try:
+                api_bp = import_module(f"apps.{app}.resources.{resource}").api_bp
+                flask_app.register_blueprint(api_bp)
+            except AttributeError:
+                pass
 
 
 def load_jobs(scheduler, logger):
